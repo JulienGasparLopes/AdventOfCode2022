979a0eda2c7857396b970464ad9f87a342193ffb
JulienGL <jgl94@free.fr>
2022-12-04 22:34:04 +0100

    end puzzle2

--- src/main/java/com/pinzen/puzzle2/Puzzle2.java
+++ src/main/java/com/pinzen/puzzle2/Puzzle2.java
@@ -4,36 +4,48 @@ import main.java.com.pinzen.Puzzle;
 
 public class Puzzle2 extends Puzzle {
 
-    private enum CHOICE {
-        ROCK("A", "X", 1, "SCISSOR"),
-        PAPER("B", "Y", 2, "ROCK"),
-        SCISSOR("C", "Z", 3, "PAPER");
+    private static int LOOSE = 0, DRAW = 3, WIN = 6;
+
+    private enum Choice {
+        ROCK("A", "X", 1, "SCISSOR", "PAPER"),
+        PAPER("B", "Y", 2, "ROCK", "SCISSOR"),
+        SCISSOR("C", "Z", 3, "PAPER", "ROCK");
 
         private String key_opp, key_myself;
         private int value;
         private String winAgainst;
+        private String looseAgainst;
 
-        CHOICE(String key_opp, String key_myself, int value, String winAgainst) {
+        Choice(String key_opp, String key_myself, int value, String winAgainst, String looseAgainst) {
             this.key_opp = key_opp;
             this.key_myself = key_myself;
             this.value = value;
             this.winAgainst = winAgainst;
+            this.looseAgainst = looseAgainst;
         }
 
         public int getScore() {
             return this.value;
         }
 
-        public int getScoreAgainst(CHOICE choice) {
+        public int getScoreAgainst(Choice choice) {
             if (choice.name().equals(this.name()))
-                return 3; // Draw
+                return DRAW;
             if (choice.name().equals(this.winAgainst))
-                return 6; // Win
-            return 0; // Loose
+                return WIN;
+            return LOOSE;
         }
 
-        public static CHOICE getChoiceFromKey(String key) {
-            for (CHOICE choice : CHOICE.values()) {
+        public Choice getChoiceForOutcome(int outcome) {
+            if (outcome == DRAW)
+                return this;
+            if (outcome == LOOSE)
+                return getChoiceFromKey(this.winAgainst);
+            return getChoiceFromKey(this.looseAgainst);
+        }
+
+        public static Choice getChoiceFromKey(String key) {
+            for (Choice choice : Choice.values()) {
                 if (choice.key_myself.equals(key) || choice.key_opp.equals(key) || choice.name().equals(key)) {
                     return choice;
                 }
@@ -41,6 +53,14 @@ public class Puzzle2 extends Puzzle {
             return null;
         }
     };
+
+    private int outcomStringToValue(String outcomeString) {
+        if (outcomeString.equals("X"))
+            return LOOSE;
+        if (outcomeString.equals("Y"))
+            return DRAW;
+        return WIN;
+    }
 
     public Puzzle2() {
         super(2);
@@ -51,11 +71,11 @@ public class Puzzle2 extends Puzzle {
         int score = 0;
         for (String line : input) {
             String[] choices = line.split(" ");
-            CHOICE choice_opp = CHOICE.getChoiceFromKey(choices[0]);
-            CHOICE choice_myself = CHOICE.getChoiceFromKey(choices[1]);
+            Choice choiceOpp = Choice.getChoiceFromKey(choices[0]);
+            Choice choiceMyself = Choice.getChoiceFromKey(choices[1]);
 
-            score += choice_myself.getScore();
-            score += choice_myself.getScoreAgainst(choice_opp);
+            score += choiceMyself.getScore();
+            score += choiceMyself.getScoreAgainst(choiceOpp);
         }
 
         return score;
@@ -63,7 +83,17 @@ public class Puzzle2 extends Puzzle {
 
     @Override
     public Object computeSecondHalfAnswer() {
+        int score = 0;
+        for (String line : input) {
+            String[] choices = line.split(" ");
+            Choice choiceOpp = Choice.getChoiceFromKey(choices[0]);
+            int outcom = outcomStringToValue(choices[1]);
 
-        return 0;
+            Choice choiceMyself = choiceOpp.getChoiceForOutcome(outcom);
+            score += choiceMyself.getScore();
+            score += choiceMyself.getScoreAgainst(choiceOpp);
+        }
+
+        return score;
     }
 }
